820e63928078cb6e6652ade76db476e0bd4cd411
Sandra <sandratorres149@gmail.com>
2019-07-21 20:51:06 +0200

    Error 1_arreglado

--- Thyroids Segmentation/Pruebas.m
+++ Thyroids Segmentation/Pruebas.m
@@ -1,8 +1,8 @@
-IMG_WIDTH  = 500
-IMG_HEIGHT = 600
+IMG_WIDTH  = 500;
+IMG_HEIGHT = 600;
 
-SUBDIVISION_NO_W  = 20
-SUBIDIVISION_NO_H = 20
+SUBDIVISION_NO_W  = 20;
+SUBIDIVISION_NO_H = 20;
 
 projectdir_us = 'D:\Thyroid_Segmentation_Papers\Images\subject_8\GE_Logiq_E9\images\';
 projectdir_gt = 'D:\Thyroid_Segmentation_Papers\Images\subject_8\GE_Logiq_E9\ground_truth\';
@@ -10,8 +10,8 @@ projectdir_gt = 'D:\Thyroid_Segmentation_Papers\Images\subject_8\GE_Logiq_E9\gro
 us_files = dir( fullfile(projectdir_us, '*.png') );
 gt_files = dir( fullfile(projectdir_gt, '*.png') );
 
-nFiles_us = length(us_files)
-nFiles_gt = length(gt_files)
+nFiles_us = length(us_files);
+nFiles_gt = length(gt_files);
 
 for i = 1:nFiles_us
     
@@ -29,16 +29,16 @@ for i = 1:nFiles_us
 
     result_vector = NaN(SUBDIVISION_NO_W * SUBIDIVISION_NO_H, 1);
     cont = 1;
-    for i = 1 to i <= h:
-        for j = 1 to j <= w:
+    for i = 1:h
+        for j = 1: w
             % Store the index of the matrix.
             x{1} = us_resized_image(i, j)
 
             % Store the matrix.
-            x{2} = us_resized_image(w:h)
+            %x{2} = us_resized_image(w:h)
 
             % Store whether if it's thyroid or not.
-            x{3} = mean(us_resized_image) > 0.6
+            %x{3} = mean(us_resized_image) > 0.6
 
             result_vector(cont) = x
 
